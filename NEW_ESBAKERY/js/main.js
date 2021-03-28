b01a15d8354665731f16c3493a4f6f7d3944badc
JeongEunBae <je_0922@naver.com>
2021-03-29 05:35:47 +0900

    메인 추가

--- NEW_ESBAKERY/js/main.js
+++ NEW_ESBAKERY/js/main.js
@@ -31,6 +31,8 @@ $(function() {
     }
   });
 
+  var window_w = $(window).innerWidth();
+
   //------- Best Seller Carousel -------//
   if($('.owl-carousel').length > 0){
     $('#bestSellerCarousel').owlCarousel({
@@ -56,6 +58,56 @@ $(function() {
     })
   }
 
+  /*------------------
+		Background set
+	--------------------*/
+	$('.set-bg').each(function() {
+		var bg = $(this).data('setbg');
+		$(this).css('background-image', 'url(' + bg + ')');
+	});
+
+	
+	/*------------------
+		Hero Slider
+	--------------------*/
+	var window_h = $(window).innerHeight();
+	var header_h = $('.header-section').innerHeight();
+	var nav_h = $('.nav-section').innerHeight();
+
+	if (window_w > 767) {
+		$('.hs-item').height((window_h)-((header_h)+(nav_h)));
+	}
+
+	$('.hero-slider').owlCarousel({
+		loop: true,
+		nav: false,
+		dots: true,
+		mouseDrag: false,
+		animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		items: 1,
+		autoplay: true
+	});
+
+  /*------------------
+		Gallery
+	--------------------*/
+	$('.gallery').find('.gallery-item').each(function() {
+		var pi_height1 = $(this).width(),
+		pi_height2 = pi_height1/2;
+		
+		if($(this).hasClass('gi-long') && window_w > 991){
+			$(this).css('height', pi_height2);
+		}else{
+			$(this).css('height', Math.abs(pi_height1));
+		}
+	});
+
+	$('.gallery').masonry({
+		itemSelector: '.gallery-item',
+		columnWidth: '.grid-sizer'
+	});
+
   //------- single product area carousel -------//
   $(".s_Product_carousel").owlCarousel({
     items:1,
@@ -65,6 +117,15 @@ $(function() {
     nav:false,
     dots:false
   });
+
+  /*------------------
+		Popup
+	--------------------*/
+	$('.img-popup').magnificPopup({
+		type: 'image',
+		mainClass: 'img-popup-warp',
+		removalDelay: 400,
+	});
 
   //------- mailchimp --------//  
 	function mailChimp() {
@@ -99,7 +160,7 @@ $(function() {
             'max': [ 10000 ]
         }
     });
-  
+    
   
     var nodes = [
         document.getElementById('lower-value'), // 0
